8336cd375b81af04d0ef4d460146919efae7433a
Essa Mustapha <essa@partnerpro.uk>
2024-11-17 12:32:20 +0400

    Initial commit

---
+++ business-dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Business Dashboard</h1>
+      <p>Welcome to your new project! Let’s start building.</p>
+    </div>
+  );
+}
+
+export default App;
